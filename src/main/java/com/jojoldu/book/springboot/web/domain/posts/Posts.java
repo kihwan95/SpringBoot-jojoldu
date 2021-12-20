d45b9ce30b66972f7d03031c13c70ce853d61642
kihwan <zpzp627@gmail.com>
2021-12-20 20:58:06 +0900

    BaseTimeEntity 상속 추가

--- src/main/java/com/jojoldu/book/springboot/web/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/web/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web.domain.posts;
 
+import com.jojoldu.book.springboot.web.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,11 @@ import lombok.ToString;
 
 import javax.persistence.*;
 
-@ToString
+
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,5 +33,10 @@ public class Posts {
         this.author = author;
     }
 
+
+    public void update(String title, String content){
+        this.title =title;
+        this.content = content;
+    }
 
 }
