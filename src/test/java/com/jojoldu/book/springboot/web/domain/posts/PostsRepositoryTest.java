d58334167270c1870a8d354dd79fba3eaaf94d8d
kihwan <zpzp627@gmail.com>
2021-12-20 21:02:18 +0900

    BaseTimeEntity(등록)Test 추가

--- src/test/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.jojoldu.book.springboot.web.domain.posts;
 
 
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -45,7 +47,43 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
-
     }
+
+         @Test
+         public void BaseTimeEntity_등록(){
+
+             //given
+             LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+             System.out.println("now:" + now);
+             postsRepository.save(Posts.builder()
+             .title("title")
+             .content("content")
+             .author("author")
+             .build());
+
+             //when
+             List<Posts> postsList = postsRepository.findAll();
+
+             //then
+             Posts posts = postsList.get(0);
+
+             System.out.println(">>>>>>>>>>>>> createDate =" + posts.getCreateDate()+", modifiedDate =" + posts.getModifiedDate());
+
+             /**
+              * isBefore() : 인자보다 과거일 때 true가 리턴
+              * isAfter() : 인자보다 미래일 때 true가 리턴
+              * isEqual() : 인자와 같은 시간일 때 true가 리턴
+              */
+             assertThat(posts.getCreateDate()).isAfter(now);
+             assertThat(posts.getModifiedDate()).isAfter(now);
+
+
+
+             //then
+
+
+         }
+
+
 
 }
