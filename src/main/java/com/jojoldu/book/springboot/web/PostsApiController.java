6bb4a446ba17345317f6d7b54e31ecad8a8ba4a4
kihwan <zpzp627@gmail.com>
2021-12-20 23:58:39 +0900

    저장 메서드 추가

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -21,6 +21,7 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        System.out.println("requestDto:" + requestDto);
         return postsService.save(requestDto);
     }
 
