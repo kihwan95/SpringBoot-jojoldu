5590fcda91980584e2a4d6222bc985de94821f30
kihwan <zpzp627@gmail.com>
2021-12-20 20:55:28 +0900

    Auditing 설정

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,13 +2,14 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * 스프링 부트의 자동설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
  * 이 어노테이션이 있는 위치부터 설정을 읽어 가기때문에 이 클래스는 항상 프로젝트의 최상단에 위치 해야 한다.
  */
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
-
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args); //내장 WAS 실행행
