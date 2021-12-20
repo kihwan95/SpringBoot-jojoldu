017947f096e6be1dc6fab62498aeac7142fb40c8
kihwan <zpzp627@gmail.com>
2021-12-20 21:02:40 +0900

    PostsResponseDto 추가

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,26 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.web.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+
+
+
+
+}
