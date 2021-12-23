dbcf23499cc054c5136caf3394b00c21fb6d83c8
kihwan <zpzp627@gmail.com>
2021-12-24 03:20:42 +0900

    세션 유저이름 추가(시큐리티)

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.web.domain.posts.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        System.out.println("user:" + user);
+
+        if(user !=null){
+            System.out.println("userName: " + user.getName());
+            model.addAttribute("googleUserName", user.getName());
+        }
 
         return "index";
     }
@@ -34,5 +46,6 @@ public class IndexController {
 
         return "posts-update";
     }
+
 
 }
