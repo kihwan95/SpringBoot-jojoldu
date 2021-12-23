f90a1317f74b6944b3f242de3787e5b6a185b57e
kihwan <zpzp627@gmail.com>
2021-12-24 03:08:08 +0900

    목록출력 추가

---
+++ src/main/java/com/jojoldu/book/springboot/web/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot.web.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
